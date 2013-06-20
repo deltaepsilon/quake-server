ce6cafffb459d172298016e18b0c6bc988cf520f
Chris Esplin <christopher.esplin@gmail.com>
2013-06-20 14:11:20 -0600

    User model

--- api/models/User.js
+++ api/models/User.js
@@ -4,24 +4,37 @@
 ---------------------*/
 module.exports = {
 
-	attributes	: { provider: 'STRING',
-  id: 'STRING',
-  displayName: 'STRING',
-  name: { familyName: 'STRING', givenName: 'STRING' },
-  emails: [ { value: 'STRING' } ],
-    _raw: 'STRING',
-  _json:
-  { id: 'STRING',
-    email: 'STRING',
-    verified_email: 'BOOLEAN',
-    name: 'STRING',
-    given_name: 'STRING',
-    family_name: 'STRING',
-    link: 'STRING',
-    picture: 'STRING',
-    gender: 'STRING',
-    birthday: 'STRING',
-    locale: 'STRING' } }
+	attributes	: {
+    provider: 'STRING',
+    id: 'STRING',
+    displayName: 'STRING',
+    name: { familyName: 'STRING', givenName: 'STRING' },
+    emails: [ { value: 'STRING' } ],
+      _raw: 'STRING',
+    _json: {
+      id: 'STRING',
+      email: 'STRING',
+      verified_email: 'BOOLEAN',
+      name: 'STRING',
+      given_name: 'STRING',
+      family_name: 'STRING',
+      link: 'STRING',
+      picture: 'STRING',
+      gender: 'STRING',
+      birthday: 'STRING',
+      locale: 'STRING'
+    },
+    stripe: {
+      publicKey: 'STRING',
+      secretKey: 'STRING'
+    },
+    subscription: {
+      active: 'BOOLEAN',
+      stripeUser: {
+
+      }
+    }
+  }
 
 
     // Simple attribute:
